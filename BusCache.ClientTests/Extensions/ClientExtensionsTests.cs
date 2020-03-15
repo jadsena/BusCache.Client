84db237a257536c332e9a692fff4a9faa8e3c7d5
Jean Alonso Duarte Sena <jadsena@hotmail.com>
2020-03-15 20:48:55 -0300

    Acertos no código e implementação de testes

---
+++ BusCache.ClientTests/Extensions/ClientExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusCache.Client.Extensions.Tests
+{
+    [TestClass()]
+    public class ClientExtensionsTests
+    {
+        [TestMethod()]
+        public void AddBusCacheClientTest()
+        {
+            //Arrange
+            var mock = new Mock<IConfiguration>();
+            ServiceCollection services = new ServiceCollection();
+            //Act
+            services.AddBusCacheClient(mock.Object);
+            //Assert
+            Assert.IsTrue(services.Count > 0);
+        }
+    }
+}
